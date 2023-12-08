f87b0459df9a091eff17167e7c5d3f00a1895fd4
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-12-08 10:12:36 +0200

    fix if in title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import { randomColor } from './randomColor';
 export const Statistics = ({ title, stats }) => {
     return (
         <section className={`${css.statistics}`}>
-            {title.length > 0 && <h2 className={`${css.title}`}>{title}</h2>}
+            {title && <h2 className={`${css.title}`}>{title}</h2>}
             <ul className={`${css.statList}`}>
           {stats.map(({ id, label, percentage }) => {
             const backgroundColor = randomColor();
